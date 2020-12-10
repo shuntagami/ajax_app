9fc0523288b126ab5d258ac97411fc5b31811abe
shuntagami <shuntagami23@gmail.com>
2020-12-10 22:46:51 +0900

    fetchAPIの利用

--- app/javascript/memo.js
+++ app/javascript/memo.js
@@ -1,41 +1,56 @@
 document.addEventListener('DOMContentLoaded', () =>  {
   const submit = document.getElementById("submit");
   submit.addEventListener("click", (e) => {
-    // formDataはfomrで入力された値を取得するオブジェクト
-    const formData = new FormData(document.getElementById("form"));
-    // コントローラーへのリクエスト？
-    const XHR = new XMLHttpRequest();
-    // ①/items/:item_id/commentsのようになったときは？(商品に対するコメント)
-    XHR.open("POST", "/posts", true);
-    XHR.responseType = "json";
-    // リクエストサーバーに送信
-    
-    XHR.send(formData);
-    console.log(formData);
-    // 応答に対するイベント
-    XHR.onload = () => {
-      // console.log(this);
-      if (XHR.status != 200) {
-        alert(`Error ${XHR.status}: ${XHR.statusText}`);
-        return null;
+  const formData = new FormData(document.getElementById("form"));
+    fetch("/posts", {
+      method: "POST",
+      body: formData,
+    }).then(response => {
+      // エラーレスポンスが返されたことを検知する
+      if (!response.ok) {
+          console.error("エラーレスポンス", response);
+      } else {
+        return response.json().then(item => {
+          const list = document.getElementById("list");
+          const formText = document.getElementById("content");
+          const HTML = escapeHTML`
+          <div class="post" data-id=${item.post.id}>
+            <div class="post-date">
+              投稿日時：${item.post.created_at}
+            </div>
+            <div class="post-content">
+              ${item.post.content}
+            </div>
+          </div>
+            `;
+            // HTMLの挿入
+          list.insertAdjacentHTML("afterend", HTML);
+          formText.value = "";
+        });
       }
-      // ②コントローラーからのレスポンスをitemという変数に代入
-      const item = XHR.response.post;
-      // console.log(XHR.response)
-      const list = document.getElementById("list");
-      const formText = document.getElementById("content");
-      const HTML = `
-        <div class="post" data-id=${item.id}>
-          <div class="post-date">
-            投稿日時：${item.created_at}
-          </div>
-          <div class="post-content">
-          ${item.content}
-          </div>
-        </div>`;
-      list.insertAdjacentHTML("afterend", HTML);
-      formText.value = "";
-    };
+    }).catch(error => {
+      console.error(error);
+      });
     e.preventDefault();
   });
- });
+});
+
+function escapeSpecialChars(str) {
+  return str
+  .replace(/&/g, "&amp;")
+  .replace(/</g, "&lt;")
+  .replace(/>/g, "&gt;")
+  .replace(/"/g, "&quot;")
+  .replace(/'/g, "&#039;");
+}
+
+function escapeHTML(strings, ...values) {
+  return strings.reduce((result, str, i) => {
+    const value = values[i - 1];
+    if (typeof value === "string") {
+        return result + escapeSpecialChars(value) + str;
+    } else {
+        return result + String(value) + str;
+    }
+  });  
+}
